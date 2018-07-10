140068194f249f8e82e8af234e8ad209901d638e
luosonglin <iluosonglin@qq.com>
2018-07-10 17:30:04 +0800

    构建项目

---
+++ app/src/main/java/app/billboardcat/com/billboardcat/MainActivity.java
@@ -0,0 +1,13 @@
+package app.billboardcat.com.billboardcat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
