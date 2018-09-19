8e155cc1d3a198adb6448087f07c60e9db01c780
luosonglin <iluosonglin@qq.com>
2018-09-19 15:52:51 +0800

    播放器

--- app/src/main/java/com/zhaopai/android/UI/MediaView/MediaDetailActivity.java
+++ app/src/main/java/com/zhaopai/android/UI/MediaView/MediaDetailActivity.java
@@ -6,6 +6,7 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.Rect;
@@ -28,10 +29,19 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.jaeger.library.StatusBarUtil;
+import com.shuyu.gsyvideoplayer.GSYVideoManager;
+import com.shuyu.gsyvideoplayer.builder.GSYVideoOptionBuilder;
+import com.shuyu.gsyvideoplayer.listener.GSYSampleCallBack;
+import com.shuyu.gsyvideoplayer.listener.LockClickListener;
+import com.shuyu.gsyvideoplayer.utils.Debuger;
+import com.shuyu.gsyvideoplayer.utils.GSYVideoType;
+import com.shuyu.gsyvideoplayer.utils.OrientationUtils;
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
 import com.zhaopai.android.MainActivity;
 import com.zhaopai.android.Network.Entity.Media;
 import com.zhaopai.android.Network.HttpData.HttpData;
 import com.zhaopai.android.R;
+import com.zhaopai.android.Util.DownloadImageTaskUtil;
 import com.zhaopai.android.Util.ToastUtils;
 
 import butterknife.BindView;
@@ -76,6 +86,8 @@ public class MediaDetailActivity extends AppCompatActivity {
     RelativeLayout toolbarHaha;
     @BindView(R.id.toolbar_name)
     TextView toolbarName;
+    @BindView(R.id.detail_player)
+    StandardGSYVideoPlayer detailPlayer;
 
     private long id;
     RequestOptions options = new RequestOptions()
@@ -88,6 +100,11 @@ public class MediaDetailActivity extends AppCompatActivity {
     // 持有这个动画的引用，让他可以在动画执行中途取消
     private Animator mCurrentAnimator;
     private int mShortAnimationDuration;
+
+    //视频
+    private boolean isPlay;
+    private boolean isPause;
+    private OrientationUtils orientationUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -185,6 +202,75 @@ public class MediaDetailActivity extends AppCompatActivity {
                         .apply(options)
                         .into(imgLocation);
                 description.setText(media.getData());
+
+                if (media.getUrl() == null || media.getUrl().equals("")) {
+                    image.setVisibility(View.VISIBLE);
+                    detailPlayer.setVisibility(View.GONE);
+                    return;
+                }
+                image.setVisibility(View.GONE);
+                detailPlayer.setVisibility(View.VISIBLE);
+
+                //增加封面
+                ImageView imageView = new ImageView(MediaDetailActivity.this);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//                imageView.setImageResource(R.mipmap.ic_launcher);
+                new DownloadImageTaskUtil(imageView).execute(media.getImgLive());
+
+                //增加title
+                detailPlayer.getTitleTextView().setVisibility(View.GONE);
+                detailPlayer.getBackButton().setVisibility(View.GONE);
+
+                //外部辅助的旋转，帮助全屏
+                orientationUtils = new OrientationUtils(MediaDetailActivity.this, detailPlayer);
+//初始化不打开外部的旋转
+                orientationUtils.setEnable(false);
+
+                GSYVideoOptionBuilder gsyVideoOption = new GSYVideoOptionBuilder();
+                gsyVideoOption.setThumbImageView(imageView)
+                        .setIsTouchWiget(true)
+                        .setRotateViewAuto(false)
+                        .setLockLand(false)
+                        .setAutoFullWithSize(true)
+                        .setShowFullAnimation(false)
+                        .setNeedLockFull(true)
+                        .setUrl(media.getUrl())
+                        .setCacheWithPlay(false)
+                        .setVideoTitle(media.getName())
+                        .setVideoAllCallBack(new GSYSampleCallBack() {
+                            @Override
+                            public void onPrepared(String url, Object... objects) {
+                                super.onPrepared(url, objects);
+                                //开始播放了才能旋转和全屏
+                                orientationUtils.setEnable(true);
+                                isPlay = true;
+                            }
+
+                            @Override
+                            public void onQuitFullscreen(String url, Object... objects) {
+                                super.onQuitFullscreen(url, objects);
+                                Debuger.printfError("***** onQuitFullscreen **** " + objects[0]);//title
+                                Debuger.printfError("***** onQuitFullscreen **** " + objects[1]);//当前非全屏player
+                                if (orientationUtils != null) {
+                                    orientationUtils.backToProtVideo();
+                                }
+                            }
+                        }).setLockClickListener((view, lock) -> {
+                            if (orientationUtils != null) {
+                                //配合下方的onConfigurationChanged
+                                orientationUtils.setEnable(!lock);
+                            }
+                        }).build(detailPlayer);
+
+                detailPlayer.getBackButton().setVisibility(View.GONE);
+
+                detailPlayer.getFullscreenButton().setOnClickListener(v -> {
+                    //直接横屏
+                    orientationUtils.resolveByClick();
+
+                    //第一个true是否需要隐藏actionbar，第二个true是否需要隐藏statusbar
+                    detailPlayer.startWindowFullscreen(MediaDetailActivity.this, true, true);
+                });
             }
 
             @Override
@@ -351,4 +437,51 @@ public class MediaDetailActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    @Override
+    public void onBackPressed() {
+        if (orientationUtils != null) {
+            orientationUtils.backToProtVideo();
+        }
+        if (GSYVideoManager.backFromWindowFull(this)) {
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        detailPlayer.getCurrentPlayer().onVideoPause();
+        super.onPause();
+        isPause = true;
+    }
+
+    @Override
+    protected void onResume() {
+        detailPlayer.getCurrentPlayer().onVideoResume(false);
+        super.onResume();
+        isPause = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (isPlay) {
+            detailPlayer.getCurrentPlayer().release();
+        }
+        if (orientationUtils != null)
+            orientationUtils.releaseListener();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        //如果旋转了就全屏
+        if (isPlay && !isPause) {
+            detailPlayer.onConfigurationChanged(this, newConfig, orientationUtils, true, true);
+        }
+    }
+
+
 }
