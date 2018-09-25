80eb67e4c433db0355b1aa2e90b0a986d5800ae0
luosonglin <iluosonglin@qq.com>
2018-09-25 17:05:00 +0800

    图片放大

--- app/src/main/java/com/zhaopai/android/UI/MediaView/MediaDetailActivity.java
+++ app/src/main/java/com/zhaopai/android/UI/MediaView/MediaDetailActivity.java
@@ -6,18 +6,25 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
@@ -28,6 +35,9 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
 import com.jaeger.library.StatusBarUtil;
 import com.shuyu.gsyvideoplayer.GSYVideoManager;
 import com.shuyu.gsyvideoplayer.builder.GSYVideoOptionBuilder;
@@ -37,6 +47,8 @@ import com.shuyu.gsyvideoplayer.utils.Debuger;
 import com.shuyu.gsyvideoplayer.utils.GSYVideoType;
 import com.shuyu.gsyvideoplayer.utils.OrientationUtils;
 import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
+import com.vondear.rxui.view.dialog.RxDialog;
+import com.vondear.rxui.view.dialog.RxDialogScaleView;
 import com.zhaopai.android.MainActivity;
 import com.zhaopai.android.Network.Entity.Media;
 import com.zhaopai.android.Network.HttpData.HttpData;
@@ -44,11 +56,14 @@ import com.zhaopai.android.R;
 import com.zhaopai.android.Util.DownloadImageTaskUtil;
 import com.zhaopai.android.Util.ToastUtils;
 
+import java.io.File;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import retrofit2.http.Url;
 
 public class MediaDetailActivity extends AppCompatActivity {
 
@@ -178,15 +193,6 @@ public class MediaDetailActivity extends AppCompatActivity {
 
             @Override
             public void onNext(Media media) {
-                Glide.with(MediaDetailActivity.this)
-                        .load(media.getImgLive())
-                        .apply(options)
-                        .into(image);
-//                image.setOnClickListener(view -> {
-//                    zoomImageFromThumb(image, media.getImgLive()); //R.mipmap.ic_launcher);
-//                });
-
-
                 name.setText(media.getName());
                 toolbarName.setText(media.getName());
 
@@ -196,10 +202,26 @@ public class MediaDetailActivity extends AppCompatActivity {
                 size.setText(media.getSize());
                 flow.setText(media.getFlow());
                 location.setText(media.getLocation());
-                Glide.with(MediaDetailActivity.this)
+
+                autoSizeBannerHeight(MediaDetailActivity.this, imgLocation);
+//                Glide.with(MediaDetailActivity.this)
+//                        .load(media.getImgLocation())
+//                        .apply(options)
+//                        .into(imgLocation);
+                Glide.with(MediaDetailActivity.this).asBitmap()
                         .load(media.getImgLocation())
-                        .apply(options)
-                        .into(imgLocation);
+                        .into(new SimpleTarget<Bitmap>() {
+                            @Override
+                            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                imgLocation.setImageBitmap(resource);
+                                imgLocation.setOnClickListener(view -> {
+                                    RxDialogScaleView rxDialogScaleView = new RxDialogScaleView(MediaDetailActivity.this);
+                                    rxDialogScaleView.setImage(resource);
+                                    rxDialogScaleView.show();
+                                });
+                            }
+                        });
+
                 description.setText(media.getData());
 
                 if (media.getUrl() == null || media.getUrl().equals("")) {
@@ -207,6 +229,11 @@ public class MediaDetailActivity extends AppCompatActivity {
                     back.setOnClickListener(view -> finish());
                     image.setVisibility(View.VISIBLE);
                     detailPlayer.setVisibility(View.GONE);
+
+                    Glide.with(MediaDetailActivity.this)
+                            .load(media.getImgLive())
+                            .apply(options)
+                            .into(image);
                     return;
                 }
                 toolbarHaha.setVisibility(View.GONE);
@@ -305,141 +332,6 @@ public class MediaDetailActivity extends AppCompatActivity {
         }
     }
 
-    private void initBigPicDialog(String url) {
-        LayoutInflater inflater = LayoutInflater.from(MediaDetailActivity.this);
-        View imgEntryView = inflater.inflate(R.layout.dialog_big_pic, null); // 加载自定义的布局文件
-        final AlertDialog dialog = new AlertDialog.Builder(MediaDetailActivity.this).create();
-        ImageView img = (ImageView) imgEntryView.findViewById(R.id.pic);
-//        加载网络图片
-        Glide.with(MediaDetailActivity.this)
-                .load(url)
-                .apply(options)
-                .into(img);
-        dialog.setView(imgEntryView); // 自定义dialog
-        dialog.show();
-
-        // 点击布局文件（也可以理解为点击大图）后关闭dialog，这里的dialog不需要按钮
-//        imgEntryView.setOnClickListener(view -> dialog.dismiss());
-    }
-
-    private void zoomImageFromThumb(final View thumbView, String url) { //int imageResId) {
-        // 如果有动画正在运行，取消这个动画
-        if (mCurrentAnimator != null) {
-            mCurrentAnimator.cancel();
-        }
-
-        // 加载显示大图的ImageView
-        final ImageView expandedImageView = (ImageView) findViewById(
-                R.id.expanded_image);
-//        expandedImageView.setImageResource(imageResId);
-        Glide.with(MediaDetailActivity.this)
-                .load(url)
-                .apply(options)
-                .into(expandedImageView);
-
-        // 计算初始小图的边界位置和最终大图的边界位置。
-        final Rect startBounds = new Rect();
-        final Rect finalBounds = new Rect();
-        final Point globalOffset = new Point();
-
-        // 小图的边界就是小ImageView的边界，大图的边界因为是铺满全屏的，所以就是整个布局的边界。
-        // 然后根据偏移量得到正确的坐标。
-        thumbView.getGlobalVisibleRect(startBounds);
-        findViewById(R.id.container).getGlobalVisibleRect(finalBounds, globalOffset);
-        startBounds.offset(-globalOffset.x, -globalOffset.y);
-        finalBounds.offset(-globalOffset.x, -globalOffset.y);
-
-        // 计算初始的缩放比例。最终的缩放比例为1。并调整缩放方向，使看着协调。
-        float startScale = 0;
-        if ((float) finalBounds.width() / finalBounds.height()
-                > (float) startBounds.width() / startBounds.height()) {
-            // 横向缩放
-            float startWidth = startScale * finalBounds.width();
-            float deltaWidth = (startWidth - startBounds.width()) / 2;
-            startBounds.left -= deltaWidth;
-            startBounds.right += deltaWidth;
-        } else {
-            // 竖向缩放
-            float startHeight = startScale * finalBounds.height();
-            float deltaHeight = (startHeight - startBounds.height()) / 2;
-            startBounds.top -= deltaHeight;
-            startBounds.bottom += deltaHeight;
-        }
-
-        // 隐藏小图，并显示大图
-        thumbView.setAlpha(0f);
-        expandedImageView.setVisibility(View.VISIBLE);
-
-        // 将大图的缩放中心点移到左上角。默认是从中心缩放
-//        expandedImageView.setPivotX(0f);
-//        expandedImageView.setPivotY(0f);
-
-        //对大图进行缩放动画
-        AnimatorSet set = new AnimatorSet();
-        set.play(ObjectAnimator.ofFloat(expandedImageView, View.X, startBounds.left, finalBounds.left))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y, startBounds.top, finalBounds.top))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X, startScale, 1f))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_Y, startScale, 1f));
-        set.setDuration(mShortAnimationDuration);
-        set.setInterpolator(new DecelerateInterpolator());
-        set.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                mCurrentAnimator = null;
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-                mCurrentAnimator = null;
-            }
-        });
-        set.start();
-        mCurrentAnimator = set;
-
-        // 点击大图时，反向缩放大图，然后隐藏大图，显示小图。
-        final float startScaleFinal = startScale;
-        expandedImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mCurrentAnimator != null) {
-                    mCurrentAnimator.cancel();
-                }
-
-                AnimatorSet set = new AnimatorSet();
-                set.play(ObjectAnimator
-                        .ofFloat(expandedImageView, View.X, startBounds.left))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.Y, startBounds.top))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.SCALE_X, startScaleFinal))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView,
-                                        View.SCALE_Y, startScaleFinal));
-                set.setDuration(mShortAnimationDuration);
-                set.setInterpolator(new DecelerateInterpolator());
-                set.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        thumbView.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
-                        mCurrentAnimator = null;
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-                        thumbView.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
-                        mCurrentAnimator = null;
-                    }
-                });
-                set.start();
-                mCurrentAnimator = set;
-            }
-        });
-    }
-
 
     @Override
     public void onBackPressed() {
@@ -485,5 +377,18 @@ public class MediaDetailActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * 根据宽度自动调整(参照图片本身尺寸)高度<br/>
+     * 需要开启 android:adjustViewBounds="true"
+     */
+    private void autoSizeBannerHeight(Context context, ImageView view) {
+
+        int imageViewWidth = (context.getResources().getDisplayMetrics().widthPixels - 24) / 2;
+
+        ViewGroup.LayoutParams lp = view.getLayoutParams();
+        lp.width = imageViewWidth;
+        lp.height = imageViewWidth / 4 * 3;//ViewGroup.LayoutParams.WRAP_CONTENT;
+        view.setLayoutParams(lp);
+    }
 
 }
